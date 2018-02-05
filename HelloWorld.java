7625b464f2882bec95aced42509bd8451ac5374f
Jason Means <jason.means@maine.edu>
2018-02-05 09:37:46 -0500

    Date issue closed

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,13 @@
+import java.util.Date;
+import java.text.*;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		Date date = new Date();
+		System.out.println(dateFormat.format(date)); 
 
 		System.out.println("hi Alice");
 
